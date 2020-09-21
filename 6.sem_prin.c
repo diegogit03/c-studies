3ac9bf8eba544754668a1f517b67fa392fa2b20b
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-09-21 20:15:00 -0300

    errors in linkeditors

---
+++ 6.sem_prin.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main(void)
+ {
+     // aqui ele nós retornará um erro de ligação, poís o comando "print" não existe.
+     print("Este programa não passa na ligação");
+     // mais sobre ligador em: https://pt.wikipedia.org/wiki/Ligador
+ }
