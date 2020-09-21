60cdad38e0c2ece3c303ee46d34a323eb95dea72
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-09-21 17:55:37 -0300

    break lines

---
+++ 4.quebra_linhas.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main(void)
+    {
+        // \n instrui ao compilador, que ele deve quebrar a linha.
+        printf("Primeira linha. \n");
+        printf("Segunda linha.");
+    }
