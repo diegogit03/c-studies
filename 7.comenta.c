02c37bdc37def6872e642973c8357e2fc89d9a36
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-09-21 20:19:16 -0300

    comments

---
+++ 7.comenta.c
@@ -0,0 +1,11 @@
+// Programa: comenta.c
+// Escrito por: Diego Henrique de Oliveira Madero
+// Data de criação: 21-09-2020
+// Proposito: Ilustrar o uso de comentários em um programa c.
+
+#include <stdio.h>
+
+void main(void)
+    {
+        printf("Bíblia do Programador C/C++, do Jamsa!"); // Exibe uma mensagem
+    }
