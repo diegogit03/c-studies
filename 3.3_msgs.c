756898be7b483f300bf2a4d3027db5a7b0632424
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-09-21 17:55:20 -0300

    add comments in messages

--- 3.3_msgs.c
+++ 3.3_msgs.c
@@ -2,6 +2,7 @@
 
 void main(void)
     {
+        // Utiliazando o printf, ele exibirá todo conteúdo em uma só linha(com os espaços)
         printf("Programando ");
         printf("em C/C++, ");
         printf("a Bíblia!");
