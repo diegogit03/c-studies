df7a907617fb07656aeef49f245423f7d12fdc4f
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-09-21 17:40:04 -0300

    error for sintax

---
+++ sintaxe.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main(void)
+    {
+        // ele me retornará um erro, poís aqui esta faltando um fechamento das aspas duplas
+        printf("Biblia do C/C++, do Jamsa!);
+    }
