a9a810f7f31cf964c17b354a2485e9b1a58ae28c
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-09-21 18:10:30 -0300

    error for lowercase in main

---
+++ 5.erro_mai.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void Main(void)
+    {
+        // o programa não passa pela compilação, poís o main esta maisculo.
+        printf("Este programa não passa pela compilação.");
+
+        // mais sobre o main:
+        // o main é o primeiro "bloco" de codigo a ser executado, ou seja é por onde o compilador começa, e o erro acima acontece,
+        // poís o compilador conhece somente "main" e não "Main".
+    }
