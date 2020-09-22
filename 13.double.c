363eb21fdc733522066961f0d6a187c6b8fec32e
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-09-22 20:43:12 -0300

    more about the double type

---
+++ 13.double.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main(void)
+    {
+        /*
+
+            o tipo double ocupa 64bits(8 bytes), ela aceita numeros decimais, e aguenta mais
+            digitos que o tipo float, de 14 a 15 são os limites.
+
+        */
+        float raio;
+        raio = 123.45; // mesmo resultado abaixo
+        raio = 1.2345E2; // Formato expodencials
+    }
