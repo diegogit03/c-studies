309165673e936c7524c6322404586a1690d36764
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-09-22 09:52:24 -0300

    more about type int

---
+++ 10.int.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main(void)
+    {
+        /*
+
+            A variavel abaixo nos da um espaço na memoria, que será do tipo int, e quando quisermos referencialá utilizaremos
+            o nome dela, que é "idadeEmAnos", outra coisa importante a se comentar sobre o tipo int, é que ele usa 16 bits na
+            memoria, 1 bit de sinal e 15 para dados.
+
+            obs: o tipo int tem que estar entre -32.768 até 32.767.
+
+        */
+        int idadeEmAnos = 20;
+    }
