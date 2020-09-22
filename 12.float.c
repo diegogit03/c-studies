d512569408b545f601a424703640b88cec8dc567
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-09-22 20:42:56 -0300

    more about the float type

---
+++ 12.float.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main(void)
+    {
+        /*
+
+            O tipo float, consome 32bits(4bytes), ele pode armazenar de 6 a 7 dígitos de precisão,
+            o C armazena como uma mantissa de 23 bits, que contem o numero fracionario.
+
+        */
+        float peso = 60.5;
+        /*
+
+            Sobre mantissa: https://pt.wikipedia.org/wiki/Significando
+
+        */
+    }
