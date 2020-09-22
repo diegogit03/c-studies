ea7a16c00ffaec3d6c37bb9ae4aa3a9ffe8b3ffc
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-09-22 09:42:11 -0300

    comments for type: open and close

---
+++ 8.comenta2.c
@@ -0,0 +1,15 @@
+/*
+
+    Programa: Comenta.c
+    Escrito por: Diego Henrique de Oliveira Madero
+    Data de Criação 21-09-2020
+    Propósito: Ilustrar o uso de comentário em um programa c.
+
+*/
+
+#include <stdio.h>
+
+void main()
+    {
+        printf("Bíblia do Programador C/C++, do Jamsa!"); /* Exibe uma mensagem */
+    }
