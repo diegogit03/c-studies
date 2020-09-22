8781702a8e090a00f694fb61a2ab7b47049a2f45
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-09-22 09:42:47 -0300

    variables basic

---
+++ 9.apresentandoVariaveis.c
@@ -0,0 +1,42 @@
+/*
+
+    Antes de começar a ler o codigo e debufa-ló, aconselho a  ler sobre variaveis primeiro:
+    http://linguagemc.com.br/variaveis-em-linguagem-c/
+
+*/
+#include <stdio.h>
+
+void main(void)
+    {
+        // as variaveis ficam aqui
+
+        // declaração
+
+        int idade;  // A idade do usuário em anos
+        int peso;   // O peso do usuário em kg
+        int altura; // A altura em centímetros
+
+        // atribuições
+
+        idade = 13;   // Atribui a idade do usuário
+        peso = 60;    // Atribui o peso
+        altura = 182; // Atribui a altura
+
+        // declarações multiplas
+
+        int idade2, peso2, altura2;
+        float salarop, impostos;
+
+        // atribuindo na declaração
+
+        int idade3 = 20;
+        int peso3 = 59;
+        int altura3 = 170;
+
+        // atribuindo a multiplas declarações
+
+        int idade4 = 5, peso4, altura4 = 106;
+
+        // outros codigos
+        printf("Bíblia do Programador C/C++, do Jamsa!");
+    }
