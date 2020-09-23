a05c4d1ea4f21a3101948999ea7054b0931b9037
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-09-23 11:46:44 -0300

    type modificar of long

---
+++ 16.modificadorDeTipoLong.c
@@ -0,0 +1,14 @@
+void main(void)
+    {
+        /*
+
+                O modificador do tipo long duz ai compilador para usar 32 bits(4 bytes)
+                para representar os valores inteiros. Uma variavel do tipo long int pode
+                armazenar valores no intervalo de -2.147.483.648 até 2.147.483.647.
+
+        */
+
+        long int valor_maximo_com_long_negativo = -2147483648;
+        long int valor_maximo_com_long_positivo = 2147483648;
+
+    }
