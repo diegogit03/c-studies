38d46285e14533832d147cce58005942749010d1
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-09-23 09:15:19 -0300

    introduction of type modificaters

---
+++ 14.modificadoresDeTipo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main(void)
+    {
+        /*
+
+            Um modificador de tipo em C aumenta um limite algum tipo, removendo algo ou adicionando.
+
+            mais sobre modificadores: http://excript.com/linguagem-c/modificador-tipo-c.html
+
+        */
+        unsigned int numero_itens;
+        register int contador;
+        long int numero_muito_grande;
+    }
