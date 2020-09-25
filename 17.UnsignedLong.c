5f395e11cd9495e0803052f0359fdef045ec8a6e
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-09-25 16:39:12 -0300

    unsigned long

--- 17.UnsignedLong.c
+++ 17.UnsignedLong.c
@@ -1,4 +1,14 @@
 void main()
     {
+        /*
 
+            Com a mistura de ambos os modificadores de tipo(unsigned e long), podemos
+            fazer a seguinte operação:
+
+            dobrar a quantia de bits e tirar um bit de sinal
+
+        */
+
+        unsigned long int valor_muito_grande;
+        unsigned long int divida_publica;
     }
