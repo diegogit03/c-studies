290e546ec6de6d8aabafad38b314598058a36d61
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-09-29 14:57:33 -0300

    short type modifiter

---
+++ 19.short.c
@@ -0,0 +1,12 @@
+void main()
+    {
+        /*
+
+            O modificador short garantirá, que o compilador
+            represente uma variavel utilizando o seu valor
+            abaixo do limite, assim não ocasionando extravasamento.
+
+        */
+        short int valor_chave;
+        short int numero_pequeno;
+    }
