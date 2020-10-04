9ff1b55c18f3bd6eb4fdcd60fca4766c9805d38d
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-10-04 10:05:44 -0300

    signed

---
+++ 21.signed.c
@@ -0,0 +1,11 @@
+void main(void)
+    {
+        /*
+
+            O modificador do tipo signed, faz o contrario do unsigned, ele garante que
+            a variavel tenha um bit de sinal.
+
+        */
+        signed char valor_que_pode_ser_positivo_ou_negativo;
+        signed char escolha_menu;
+    }
