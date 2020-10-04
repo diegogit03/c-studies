f4ece3ea36a4f583809b0998f3b1ab28c569d1f6
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-10-04 10:07:28 -0300

    multi assignment

---
+++ 22.multiplasOperaçõesDeAtribuição.c
@@ -0,0 +1,16 @@
+void main(void)
+    {
+        /*
+
+            Em c temos varias possibilidades para fazer atribuições a
+            muitas variaveis olhe abaixo algumas:
+
+        */
+
+        conta = 0;
+        soma = 0;
+        valor = 0;
+
+        // mesmo resuldado das variaveis acima
+        conta_multipla_atribuicao = soma_multipla_atribuicao = valor_multipla_atribuicao = 0;
+    }
