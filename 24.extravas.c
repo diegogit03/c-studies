f58a4a6c65a39835eaaf1a70bd72533acc10e7c5
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-10-07 14:26:32 -0300

    about for spills

---
+++ 24.extravas.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main(void)
+    {
+        int positivo = 32767;
+        int negativo = -32768;
+
+        printf("%d + 1 = %d\n", positivo, positivo+1);
+        printf("%d - 1 = %d\n", negativo, negativo-1);
+    }
