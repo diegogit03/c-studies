2ee2ed582410b9dc27e9bda63fb76d1456c85f32
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-10-15 11:00:54 -0300

    printf introduction

---
+++ 26.printf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main(void)
+    {
+        float flutuante = 1.2;
+        // obs: a string precisa ser com aspas duplas
+        printf("\na expressao e: %d\no ponto flutuante e: %f\n", /*expressão*/3*2, /*ponto flutuante*/flutuante);
+    }
