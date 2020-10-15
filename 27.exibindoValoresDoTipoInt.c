a00bb24696f0180a444b0131152fa68eb3ef344a
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-10-15 11:01:07 -0300

    sohw int values

---
+++ 27.exibindoValoresDoTipoInt.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main(void)
+    {
+        int idade = 41;
+        int altura = 182;
+        int peso = 80;
+
+        printf("\nIdade do usuário: %d peso: %d altura: %d\n", idade, peso, altura);
+        printf("%d mais %d é igual a %d\n", 1, 2, 1+2);
+    }
