3b4c2ec4f9a26206d9329337db2873454f6399f6
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-10-23 16:23:51 -0300

    show lont int values in printf

--- 29.u_intsai.c
+++ 29.u_intsai.c
@@ -6,4 +6,5 @@ void main(void)
 
 		printf("Exibindo 42000 como unsigned %u\n", valor);
 		printf("Exibindo 42000 como int ou decimal %d", valor);
+		// obs: o extravamento muda nos sistemas 64 bits mais modernos
 	}
---
+++ 30.longsai.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main()
+	{
+		long int um_milhao = 1000000;
+
+		printf("Um milhao e %ld\n", um_milhao);
+		printf("Um milhao e %d", um_milhao);
+	}
