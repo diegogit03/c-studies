19bf1ef179d9164b19294dbc259fa068f6ca7172
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-10-23 16:16:09 -0300

    show unsigned int in printf

---
+++ 29.u_intsai.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main(void)
+	{
+		unsigned int valor = 42000;
+
+		printf("Exibindo 42000 como unsigned %u\n", valor);
+		printf("Exibindo 42000 como int ou decimal %d", valor);
+	}
