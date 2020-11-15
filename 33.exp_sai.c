52e5e99889430d166fd30d0ccabdc074ab39e77a
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-11-15 15:23:38 -0300

    show expotential values

---
+++ 33.exp_sai.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main(void)
+	{
+		float pi = 3.14159;
+		float raio = 2.0031;
+
+		printf("A área do círculo é %e\n", 2 * pi * raio);
+		printf("A área do círculo é %E\n", 2 * pi * raio);
+	}
