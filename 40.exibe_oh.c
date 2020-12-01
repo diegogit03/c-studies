7bcea0eac8fb55b6e9b6db739ba8c935a785b895
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-12-01 17:30:52 -0300

    show prefix in octais and hexa values

---
+++ 40.exibe_oh.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main()
+	{
+		int valor = 255;
+
+		printf("O valor decimal %d em octal e %#o\n", valor, valor);
+		printf("O valor decimal %d em hexadecimal e %#x\n", valor, valor);
+		printf("O valor decimal %d em hexadecimal e %#X\n", valor, valor);
+	}
