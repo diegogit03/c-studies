d7975953f87312e4759a1b5962bd1294688f812d
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-12-05 13:29:55 -0300

    float formated

---
+++ 041.flt_fmt.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main()
+    {
+        float valor = 1.23456;
+
+        printf("%8.1f\n", valor);
+        printf("%8.3f\n", valor);
+        printf("%8.5f\n", valor);
+    }
