140d1d868af9c12e351d0639c57274107440f9b8
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-12-05 13:30:14 -0300

    expotential float

---
+++ 042.exp_fmt.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main()
+    {
+        float valor = 1.23456;
+
+        printf("%12.1e\n", valor);
+        printf("%12.3e\n", valor);
+        printf("%12.5e\n", valor);
+    }
