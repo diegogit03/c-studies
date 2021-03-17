887f752749b75184643cc300cacd43e7d992601f
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2021-03-17 16:20:38 -0300

    prt_cnt and codepen config file

---
+++ 047.prt_cnt.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main(void)
+	{
+		int primeiro_conta;
+		int segundo_conta;
+
+		printf("Bíblia%n do C/C++, do Jamsa!%n\n", &primeiro_conta, &segundo_conta);
+		printf("Primeiro conta %d segundo conta %d\n", primeiro_conta, segundo_conta);
+	}
