7c49694ca31e8bd60ef07b5e3186ed6d2b7a0974
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2021-03-19 17:05:08 -0300

    or bit

---
+++ 054.ou_bit.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main(void)
+{
+	printf("0 | 0 é %d\n", 0 | 0);
+	printf("0 | 1 é %d\n", 0 | 1);
+	printf("1 | 1 é %d\n", 1 | 1);
+	printf("1 | 2 é %d\n", 1 | 2);
+	printf("128 | 127 é %d\n", 128 | 127);
+}
